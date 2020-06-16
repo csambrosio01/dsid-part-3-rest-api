9abcd5e6f888c4881e69654f7ab579e75eb173c5
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-16 15:11:43 -0300

    Create User and CreateUser model

---
+++ app/model/User.scala
@@ -0,0 +1,30 @@
+package model
+
+import java.sql.Timestamp
+
+import play.api.libs.json.{Json, OFormat, Reads}
+import format.TimestampFormat._
+
+case class User(
+                 userId: Option[Long],
+                 username: String,
+                 password: String,
+                 name: String,
+                 email: String,
+                 phoneNumber: Long,
+                 createdAt: Timestamp,
+                 updatedAt: Timestamp
+               )
+
+case class CreateUser(
+                       username: String,
+                       password: String,
+                       name: String,
+                       email: String,
+                       phoneNumber: Long
+                     )
+
+object User {
+  implicit val userFormat: OFormat[User] = Json.format[User]
+  implicit val createUserRead: Reads[CreateUser] = Json.reads[CreateUser]
+}
