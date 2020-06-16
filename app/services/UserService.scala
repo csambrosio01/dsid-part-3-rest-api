304e9e8fc1f69c891f7713d072a59b2f36b105ad
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-16 15:17:33 -0300

    Add classes to handle user session and cookies
    This classes where orriginally forked from projet play-scala-secure-session-example developed by Play Dev Team, but modified at project locker-demo developed by Caio Ambrosio.
    Links to projects:
     - https://github.com/playframework/play-samples/tree/2.8.x/play-scala-secure-session-example
     - https://github.com/csambrosio01/DSID-Play/tree/master/locker-demo

---
+++ app/services/UserService.scala
@@ -0,0 +1,38 @@
+package services
+
+import exception.{PasswordException, WrongCredentialsException}
+import javax.inject.Inject
+import model.{CreateUser, Login, User}
+import persistance.user.UserRepository
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class UserService @Inject()(
+                             userRepository: UserRepository
+                           )
+                           (
+                             implicit ec: ExecutionContext
+                           ) {
+
+  def createUser(user: CreateUser): Future[User] = {
+    if (user.password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}\\[\\]:;<>,.?/~_+-=|\\\\]).{8,40}$")) {
+      userRepository.create(user)
+    } else {
+      throw PasswordException("Wrong password expression")
+    }
+  }
+
+  def login(login: Login): Future[User] = {
+    userRepository
+      .findUserByUsername(login.username)
+      .map { user =>
+        user.fold(throw WrongCredentialsException("Wrong credentials")) { user =>
+          if (user.password == login.password) {
+            user
+          } else {
+            throw WrongCredentialsException("Wrong credentials")
+          }
+        }
+      }
+  }
+}
