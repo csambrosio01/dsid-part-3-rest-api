79da868f301614e62a2991fe63e78d5b3e4f7c2d
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-16 15:58:49 -0300

    Add missing dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,6 @@
 import sbt._
 import play.sbt.PlayImport._
+import play.core.PlayVersion.akkaVersion
 
 object Dependencies {
   private val scalaTest = "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
@@ -9,7 +10,12 @@ object Dependencies {
   private val playSlickEvolutions = "com.typesafe.play" %% "play-slick-evolutions" % "5.0.0"
   private val postgres = "org.postgresql" % "postgresql" % "42.2.14"
 
+  //Session
+  private val kalium = "org.abstractj.kalium" % "kalium" % "0.8.0"
+  private val akkaDistributedData = "com.typesafe.akka" %% "akka-distributed-data" % akkaVersion
+  private val akkaClusterTyped = "com.typesafe.akka" %% "akka-cluster-typed" % akkaVersion
+
   val dependencies = Seq(
-    guice, scalaTest, playSlick, playSlickEvolutions, postgres
+    guice, scalaTest, playSlick, playSlickEvolutions, postgres, kalium, akkaDistributedData, akkaClusterTyped
   )
 }
