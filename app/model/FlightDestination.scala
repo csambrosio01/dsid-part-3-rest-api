ef400ed484e7f5b454b62230302ae4f436bc0baa
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-06-19 10:51:52 -0300

    Add models for FlightDestination endpoint
    Models:
     - FlightDestinationResult: used to transform Amadeus response to scala class
     - FlightDestination: model that Amadeus return
     - Price: used in FlightDestination
     - FlightLinks: used in FlightDestination
    
    Also add format for each model

---
+++ app/model/FlightDestination.scala
@@ -0,0 +1,33 @@
+package model
+
+import java.util.Date
+
+import play.api.libs.json.{Json, OFormat}
+
+case class FlightDestinationResult(data: Seq[FlightDestination])
+
+case class FlightDestination(
+                              `type`: String,
+                              origin: String,
+                              destination: String,
+                              departureDate: Date,
+                              returnDate: Date,
+                              price: Price,
+                              links: FlightLinks
+                            )
+
+case class Price(
+                  total: String
+                )
+
+case class FlightLinks(
+                        flightDates: String,
+                        flightOffers: String
+                      )
+
+object FlightDestination {
+  implicit val flightLinksFormat: OFormat[FlightLinks] = Json.format[FlightLinks]
+  implicit val priceFormat: OFormat[Price] = Json.format[Price]
+  implicit val flightDestinationFormat: OFormat[FlightDestination] = Json.format[FlightDestination]
+  implicit val flightDestinationResultFormat: OFormat[FlightDestinationResult] = Json.format[FlightDestinationResult]
+}
