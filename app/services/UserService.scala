b1142a6a35708cb7ca01bede0e1180ebbb9cf040
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-07-03 18:20:37 -0300

    Add method on UserService to search address by zipcode on correios soap web service

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -1,18 +1,22 @@
 package services
 
-import exception.{PasswordException, WrongCredentialsException}
+import exception.{NotFoundException, PasswordException, WrongCredentialsException}
 import javax.inject.Inject
-import model.{CreateUser, Login, User}
+import model.{AddressResponse, CreateUser, Login, User}
 import persistance.user.UserRepository
+import play.api.libs.ws.WSClient
+import requests.BaseExternalRequests
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class UserService @Inject()(
-                             userRepository: UserRepository
+                             userRepository: UserRepository,
+                             ws: WSClient
                            )
                            (
                              implicit ec: ExecutionContext
-                           ) {
+                           )
+  extends BaseExternalRequests(ws) {
 
   def createUser(user: CreateUser): Future[User] = {
     if (user.password.matches("""^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#(){}?:;><=.,^~_+-\[\]])[A-Za-z\d@$!%*?&#(){}?:;><=.,^~_+-\[\]]{8,40}$""")) {
@@ -32,6 +36,37 @@ class UserService @Inject()(
           } else {
             throw WrongCredentialsException("user.login.wrong_credentials")
           }
+        }
+      }
+  }
+
+  def getZipCode(zipCode: String): Future[AddressResponse] = {
+    val postBody =
+      s"""
+                   <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                   <Body>
+                   <consultaCEP xmlns="http://cliente.bean.master.sigep.bsb.correios.com.br/">
+                   <cep xmlns="">$zipCode</cep>
+                   </consultaCEP>
+                   </Body>
+                   </Envelope>"""
+
+    prepareFullRequest("https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl", 10 * 60)
+      .addHttpHeaders("Content-Type" -> "text/xml")
+      .post(postBody)
+      .map { result =>
+        if (result.status == 200) {
+          val response = result.xml
+          AddressResponse(
+            (response \\ "end").text,
+            (response \\ "bairro").text,
+            (response \\ "cidade").text,
+            (response \\ "uf").text,
+            (response \\ "cep").text,
+            "Brasil"
+          )
+        } else {
+          throw NotFoundException("cep.not_found")
         }
       }
   }
